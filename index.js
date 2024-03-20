742008ac44810efb1e89c6e3b5a7041d42fe0b2f
Maham Babar <mahambabar11@gmail.com>
2024-03-20 05:13:36 -0700

    typescript

---
+++ index.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter First number", type: "number", name: "firstNumber" },
+    { message: "Enter Second number", type: "number", name: "secondNumber" },
+    { message: "select the operator", type: "list", name: "operator", choices: ["Addition", "subtraction", "Multiplication", "Division"] }
+]);
+//CONDITIONAL STATEMENT:
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division ") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Assign a valid operator");
+}
